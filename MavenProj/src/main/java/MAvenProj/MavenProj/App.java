401ce2eeefb3a6a2870b8c7a59f4ff231c8794a8
DELL <DELL@DELL-PC>
2019-07-21 17:26:15 +0530

    transfering the file

---
+++ MavenProj/src/main/java/MAvenProj/MavenProj/App.java
@@ -0,0 +1,13 @@
+package MAvenProj.MavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MavenProj/src/test/java/DemoProj/FirstProj.java
@@ -0,0 +1,11 @@
+package DemoProj;
+
+public class FirstProj {
+@Test
+public void sum()
+{
+	int a= 10;
+	int b=20;
+	Assert.
+}
+}
